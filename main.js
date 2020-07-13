1913ee9ca0969699ee7c80eb65b09211d89067bf
Ryan Roat <ryanroat@gmail.com>
2020-07-13 15:27:50 -0400

    fix typos/move onload call

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
+const canvas = document.getElementById('gameCanvas');
+const canvasContext = canvas.getContext('2d');
+
+// target frames per second rate
+const FPS = 30;
+// initial location and size of ball
+let ballX = 50;
+let ballY = 50;
+const ballRadius = 10;
+// initial ball vectors
+let ballSpeedX = 8;
+let ballSpeedY = 4;
+
+// paddle details
+const paddle1Y = 250;
+const paddle1Height = 100;
+
+// mouse position
+
+function calcMousePos(evt) {
+    const rect = canvas.getBoundingClientRect();
+    const root = document.documentElement;
+    const mouseX = evt.clientX - rect.left - root.scrollLeft;
+    const mouseY = evt.clientY - rect.top - root.scrollTop;
+    return {
+        x: mouseX,
+        y: mouseY
+    };
+}
+
+// draw canvas rectangle of given location, size and color
+function colorRect(leftX, topY, width, height, drawColor) {
+    canvasContext.fillStyle = drawColor;
+    canvasContext.fillRect(leftX, topY, width, height);
+}
+
+// draw canvas circle of given location, radius and color
+function colorCircle(centerX, centerY, radius, drawColor) {
+    canvasContext.fillStyle = drawColor;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+    canvasContext.fill();
+}
+
 window.onload = () => {
     console.log('document loaded');
-
-    const canvas = document.getElementById('gameCanvas');
-    const canvasContext = canvas.getContext('2d');
-
-    // target frames per second rate
-    const FPS = 30;
-    // initial location and size of ball
-    let ballX = 50;
-    let ballY = 50;
-    const ballRadius = 10;
-    // initial ball vectors
-    let ballSpeedX = 8;
-    let ballSpeedY = 4;
-
-    // paddle details
-    const paddle1Y = 250;
-    const paddle1Height = 100;
-
-    // draw canvas rectangle of given location, size and color
-    function colorRect(leftX, topY, width, height, drawColor) {
-        canvasContext.fillStyle = drawColor;
-        canvasContext.fillRect(leftX, topY, width, height);
-    }
-
-    // draw canvas circle of given location, radius and color
-    function colorCircle(centerX, centerY, radius, drawColor) {
-        canvasContext.fillStyle = drawColor;
-        canvasContext.beginPath();
-        canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
-        canvasContext.fill();
-    }
-
     // updates canvas
     drawField = () => {
         // draw blank black playing field
