0b8fcd657677b9c160442cdd9051c7fea47a3323
Ryan Roat <ryanroat@gmail.com>
2020-07-14 14:26:36 -0400

    reverse ball on reset with rand speed, refactor

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ let ballY;
 const ballRadius = 10;
 let ballSpeedX;
 let ballSpeedY;
+// let serve;
 
 // paddle details
 const paddleWidth = 10;
@@ -51,6 +52,8 @@ function colorCircle(centerX, centerY, radius, drawColor) {
 
 // initialize ball speed and direction
 function ballInit() {
+    // serve indicates ball X direction as -1 or 1
+    // serve =
     // radomize ballSpeed X between 10 & 15
     ballSpeedX = rando(6) + 9 * (Math.random() < 0.5 ? -1 : 1);
     // randomize ballSpeedY between -10 & 10
@@ -62,62 +65,70 @@ function ballInit() {
 // reset ball location to center of canvas
 function ballReset() {
     // reverse ballSpeedX
-    ballSpeedX *= -1;
+    ballSpeedX = -(ballSpeedX / Math.abs(ballSpeedX)) * (rando(6) + 9);
     // randomize ballSpeedY between -10 & 10
     ballSpeedY = rando(21) - 11;
     ballX = canvas.width / 2;
     ballY = canvas.height / 2;
 }
 
+function drawField() {
+    // draw blank black playing field
+    colorRect(0, 0, canvas.width, canvas.height, 'black');
+    // draw left player paddle
+    colorRect(0, paddle1Y, paddleWidth, paddle1Height, 'white');
+    // draw right player paddle
+    colorRect(
+        canvas.width - paddleWidth,
+        paddle2Y,
+        paddleWidth,
+        paddle2Height,
+        'white'
+    );
+    // draw white ball
+    colorCircle(ballX, ballY, ballRadius, 'white');
+}
+
+function movement() {
+    // move ball by x vector
+    ballX += ballSpeedX;
+    // reset ball location if it reaches left edge
+    if (ballX < 0) {
+        if (ballY > paddle1Y && ballY < paddle1Y + paddle1Height) {
+            ballSpeedX *= -1;
+        } else {
+            ballReset();
+        }
+    }
+    // reset ball location if it reaches right edge
+    if (ballX > canvas.width) {
+        if (ballY > paddle2Y && ballY < paddle2Y + paddle2Height) {
+            ballSpeedX *= -1;
+        } else {
+            ballReset();
+        }
+    }
+
+    // move ball by y vector
+    ballY += ballSpeedY;
+    // if ball hits edge of field, reverse y vector
+    if (ballY < ballRadius || ballY > canvas.height - ballRadius) {
+        ballSpeedY *= -1;
+    }
+}
+
 window.onload = () => {
     console.log('document loaded');
     //
     ballInit();
-    // updates canvas
-    drawField = () => {
-        // draw blank black playing field
-        colorRect(0, 0, canvas.width, canvas.height, 'black');
-        // draw left player paddle
-        colorRect(0, paddle1Y, paddleWidth, paddle1Height, 'white');
-        // draw right player paddle
-        colorRect(
-            canvas.width - paddleWidth,
-            paddle2Y,
-            paddleWidth,
-            paddle2Height,
-            'white'
-        );
-        // draw white ball
-        colorCircle(ballX, ballY, ballRadius, 'white');
-        // move ball by x vector
-        ballX += ballSpeedX;
-        // reset ball location if it reaches left edge
-        if (ballX < 0) {
-            if (ballY > paddle1Y && ballY < paddle1Y + paddle1Height) {
-                ballSpeedX *= -1;
-            } else {
-                ballReset();
-            }
-        }
-        // reset ball location if it reaches right edge
-        if (ballX > canvas.width) {
-            if (ballY > paddle2Y && ballY < paddle2Y + paddle2Height) {
-                ballSpeedX *= -1;
-            } else {
-                ballReset();
-            }
-        }
-
-        // move ball by y vector
-        ballY += ballSpeedY;
-        // if ball hits edge of field, reverse y vector
-        if (ballY < ballRadius || ballY > canvas.height - ballRadius) {
-            ballSpeedY *= -1;
-        }
+    const updateField = () => {
+        // updates canvas
+        drawField();
+        movement();
     };
 
     // draw field at FPS rate
-    setInterval(drawField, 1000 / FPS);
+    setInterval(updateField, 1000 / FPS);
 
     // move paddle1 to track mouse
     canvas.addEventListener('mousemove', function(evt) {
